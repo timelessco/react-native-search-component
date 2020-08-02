13d21d20967cf1de305517ed5f6d6cde2a394167
Karthik-B-06 <karthik06051996@gmail.com>
2020-08-02 21:50:05 +0530

    :tada: added SearchBar component to App.js

--- example/RNSearch/App.js
+++ example/RNSearch/App.js
@@ -8,19 +8,41 @@
 
 import React from 'react';
 import {
-  SafeAreaView,
-  StatusBar
+  Button, StyleSheet,
+  Text, View, SafeAreaView, TouchableOpacity
 } from 'react-native';
+import SearchBar from './src/UISearchBar';
 
 
 const App = () => {
+  const [theme, setTheme] = React.useState('LIGHT');
+  const toggleTheme = () => theme === 'LIGHT' ? setTheme('DARK') : setTheme('LIGHT');
+  const themeBasedContainer = [styles.container, { backgroundColor: theme === 'LIGHT' ? 'white' : 'black' }];
+  const themeBasedTextStyle = [styles.textStyle, { color: theme === 'LIGHT' ? 'black' : 'white' }];
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-      </SafeAreaView>
-    </>
+    <SafeAreaView style={themeBasedContainer}>
+      <Text style={themeBasedTextStyle}>Hello,World !</Text>
+      <TouchableOpacity style={{ paddingVertical: 12 }} onPress={toggleTheme}>
+        <Text style={[styles.textStyle, { color: '#007AFF' }]}>Toggle Theme</Text>
+      </TouchableOpacity>
+      <SearchBar
+        theme={theme}
+      />
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    paddingHorizontal: 16,
+  },
+  textStyle: {
+    fontSize: 24,
+    textAlign: 'center',
+    paddingVertical: 10
+  }
+})
 
 export default App;
