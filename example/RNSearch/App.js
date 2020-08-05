b7ca4dec6a50ea7a13a56742b0919055c452c4b9
Karthik-B-06 <karthik06051996@gmail.com>
2020-08-05 23:21:07 +0530

    :tada: Added search/close icons, added onSearchClear and onChange props

--- example/RNSearch/App.js
+++ example/RNSearch/App.js
@@ -6,7 +6,7 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 import {
   SafeAreaView, StyleSheet,
   Text, TouchableOpacity
@@ -16,16 +16,23 @@ import SearchBar from './src/UISearchBar';
 
 const App = () => {
   const [theme, setTheme] = React.useState('LIGHT');
+  const [searchTerm, setSearchTerm] = useState('');
   const toggleTheme = () => theme === 'LIGHT' ? setTheme('DARK') : setTheme('LIGHT');
   const themeBasedContainer = [styles.container, { backgroundColor: theme === 'LIGHT' ? 'white' : 'black' }];
   const themeBasedTextStyle = [styles.textStyle, { color: theme === 'LIGHT' ? 'black' : 'white' }];
+  const onChange = (e) => {
+    setSearchTerm(e?.nativeEvent?.text)
+  }
+  const onSearchClear = () => setSearchTerm('');
+
   return (
     <SafeAreaView style={themeBasedContainer}>
       <Text style={themeBasedTextStyle}>Hello,World !</Text>
       <TouchableOpacity style={{ paddingVertical: 12 }} onPress={toggleTheme}>
         <Text style={[styles.textStyle, { color: '#007AFF' }]}>Toggle Theme</Text>
       </TouchableOpacity>
-      <SearchBar theme={theme} />
+      <SearchBar theme={theme} onChange={onChange} onSearchClear={onSearchClear} />
+      <Text style={[themeBasedTextStyle, { textAlign: 'left', paddingLeft: 16 }]}> Search Term : {searchTerm}</Text>
     </SafeAreaView>
   );
 };
