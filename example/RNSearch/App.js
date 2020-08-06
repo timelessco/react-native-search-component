0f78e18d8ba96691d660574caaa224713c95609f
Karthik-B-06 <karthik06051996@gmail.com>
2020-08-06 13:41:37 +0530

    style: :lipstick: updated styles

--- example/RNSearch/App.js
+++ example/RNSearch/App.js
@@ -21,12 +21,12 @@ const App = () => {
 
   return (
     <SafeAreaView style={themeBasedContainer}>
-      <Text style={themeBasedTextStyle}>Hello,World !</Text>
+      <Text style={themeBasedTextStyle}>React Native Search Component</Text>
       <TouchableOpacity style={{ paddingVertical: 12 }} onPress={toggleTheme}>
-        <Text style={[styles.textStyle, { color: '#007AFF' }]}>Toggle Theme</Text>
+        <Text style={[styles.textStyle, { color: '#007AFF', fontSize: 18 }]}>Toggle Theme</Text>
       </TouchableOpacity>
       <SearchBar value={searchTerm} theme={theme} onChange={onChange} onSearchClear={onSearchClear} />
-      <Text style={[themeBasedTextStyle, { textAlign: 'left', paddingLeft: 16 }]}> Search Term : {searchTerm}</Text>
+      <Text style={[themeBasedTextStyle, { textAlign: 'left', paddingLeft: 16, fontSize: 18 }]}> Search Term : {searchTerm}</Text>
     </SafeAreaView>
   );
 };
@@ -34,6 +34,7 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center'
   },
   textStyle: {
     fontSize: 24,
